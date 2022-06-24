77909fb7368f339a4739934d257ae33d76c773e7
Bartek Kuropatwa <bartek.kuropatwa@gmail.com>
2022-06-24 21:02:00 +0200

    Initial version

--- kodilla-intro/src/main/java/FirstClass.java
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class FirstClass {
+public class FirstClass {
+    public static void main(String[] args) {
+        String hello = "Hello World!";
+        String name = "Bartek";
+        int age = 26;
+        char lastName = 'K';
+        System.out.println(hello + " " + name + " " + age + " " + lastName);
+
+
+    }
 }
