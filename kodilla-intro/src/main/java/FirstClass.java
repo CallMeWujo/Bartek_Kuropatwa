a1c04cb883fdc6d7efa3172f1683d1b829aedf68
Bartek Kuropatwa <bartek.kuropatwa@gmail.com>
2022-06-24 21:03:21 +0200

    pierwsze zadanie

--- kodilla-intro/src/main/java/FirstClass.java
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -2,7 +2,7 @@ public class FirstClass {
     public static void main(String[] args) {
         String hello = "Hello World!";
         String name = "Bartek";
-        int age = 26;
+        int age = 27;
         char lastName = 'K';
         System.out.println(hello + " " + name + " " + age + " " + lastName);
 
