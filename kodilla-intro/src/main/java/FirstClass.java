34a13e57031035119ba427507643bb0376bd65c9
Bartek Kuropatwa <bartek.kuropatwa@gmail.com>
2022-06-27 22:16:27 +0200

    kodilla

--- kodilla-intro/src/main/java/FirstClass.java
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -1,6 +1,12 @@
 public class FirstClass {
     public static void main(String[] args) {
-        String example = "Hello from FirstClass!";
-        System.out.println(example);
+        System.out.print("Hello from FirstClass ");
+
+        if (1>2){
+            System.out.println("Niesamowite! Jeden jest wieksze niz dwa!!!");
+        }
+        else {
+            System.out.println("Wszystko po staremu, jeden jest mniejsze niz dwa");
+        }
     }
 }
