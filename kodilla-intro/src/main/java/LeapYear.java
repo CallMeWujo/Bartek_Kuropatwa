7a8e595564670b42ecf46dde75530c23641a05ff
Bartek Kuropatwa <bartek.kuropatwa@gmail.com>
2022-06-30 22:04:58 +0200

    kodilla

---
+++ kodilla-intro/src/main/java/LeapYear.java
@@ -0,0 +1,20 @@
+public class LeapYear {
+    public static void main(String[] args) {
+        int year = 2022;
+
+        if(year % 4 == 0 && year % 100 == 0 && year % 400 == 0) {
+            System.out.println("is  leap year");
+        }
+        else if (year % 4 == 0 && year % 100 == 0 || year % 400 == 1) {
+            System.out.println("is not leap year");
+        }
+        else if (year % 4 == 0) {
+            System.out.println("is leap year");
+        }
+        else {
+            System.out.println("is not leap year");
+        }
+
+
+    }
+}
