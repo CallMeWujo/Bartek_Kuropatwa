43d987951e11822c590f136ce7c36174a30ec93c
Bartek Kuropatwa <bartek.kuropatwa@gmail.com>
2022-07-02 14:37:39 +0200

    kodilla

---
+++ kodilla-intro/src/main/java/SimpleArray.java
@@ -0,0 +1,10 @@
+public class SimpleArray {
+    public static void main(String[] args) {
+        String[] books = new String[5];
+        books[0] = "Hobbit";
+        books[1] = "Silmarillion";
+        books[2] = "Star Wars Darth Bane - Zasada Dwóch";
+        books[3] = "Dzieci Hurina";
+        books[4] = "Pan Lodowego Ogrodu";
+    }
+}
