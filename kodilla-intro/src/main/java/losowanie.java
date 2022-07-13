2951b6bd155c32c9ec51bfa87b6e9a10bec4c4d7
Bartek Kuropatwa <bartek.kuropatwa@gmail.com>
2022-07-13 17:12:16 +0200

    kodilla

---
+++ kodilla-intro/src/main/java/losowanie.java
@@ -0,0 +1,17 @@
+public class losowanie {
+    public static int getCountOfRandomNumber(int max) {
+        Random random = new Random();
+        int result = 0;
+        int sum = 0;
+        while (sum < max) {
+            int temp = random.nextInt(10);
+            sum = sum + temp;
+            result++;
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        int randomNumbers  = getCountOfRandomNumber(10);
+                System.out.println(randomNumbers);
+    }
+}
---
+++ kodilla-intro/src/main/java/zwracanie.java
@@ -0,0 +1,13 @@
+public class zwracanie {
+   public static int sum(int a, int b) {
+       int result = a + b;
+       return result;
+
+   }
+   public static void main(String[] args) {
+       int calculatedSum = sum(1,2);
+       System.out.println(calculatedSum);
+
+   }
+}
+
