a4d80e158f3765aa597f027880a7227e342e76b6
Bartek Kuropatwa <bartek.kuropatwa@gmail.com>
2022-08-09 20:43:42 +0200

    kodilla 2.6 obiektowosc

--- kodilla-intro/src/main/java/Application.java
+++ kodilla-intro/src/main/java/Application.java
@@ -1,2 +1,6 @@
-package PACKAGE_NAME;public class Application {
+public class Application {
+    public static void main(String[] args) {
+        UserDetails Name = new UserDetails("Adam", 40.5, 178);
+        Name.checkDetails();
+    }
 }
--- kodilla-intro/src/main/java/UserDetails.java
+++ kodilla-intro/src/main/java/UserDetails.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class UserDetails {
+public class UserDetails {
+
+        String name;
+        double age;
+        double height;
+
+    public UserDetails (String name, double age, double height) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+    public void checkDetails() {
+        if (name !=null) {
+            if (age > 30 && height > 160) {
+                System.out.println("User is older than 30 and taller than 160cm");
+            } else {
+                System.out.println("User is 30 (or younger) or 160cm (or shorter)");
+            }
+        }
+    }
+
 }
