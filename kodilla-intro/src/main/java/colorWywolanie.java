eca729d3dc1ea0fa3d19f6f6b79888d0bc7ace56
CallMeWujo <108149963+CallMeWujo@users.noreply.github.com>
2022-08-09 20:32:54 +0200

    Delete colorWywolanie.java

--- kodilla-intro/src/main/java/colorWywolanie.java
+++
@@ -1,7 +0,0 @@
-public class colorWywołanie {
-    public static void main(String[] args) {
-        Color userColor = new Color();
-        String result = userColor.colorSelection();
-        System.out.println(result);
-    }
-}
