48c3ee0126cb3f54ba5024dba62b1c3fbdc126b2
Bartek Kuropatwa <bartek.kuropatwa@gmail.com>
2022-09-23 20:46:31 +0200

    kodilla 6.3

--- kodilla-stream/src/main/java/com/kodilla/stream/homework/ForumStats.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/homework/ForumStats.java
@@ -1,2 +1,42 @@
-package com.kodilla.stream.homework;public class ForumStats {
+package com.kodilla.stream.homework;
+
+import com.kodilla.stream.User;
+import com.kodilla.stream.UsersRepository;
+import org.w3c.dom.ls.LSInput;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ForumStats {
+    public static double getAverageNumberOfPostsForPeopleOlderOrEqualToFortyYearsOfAge(List<User> users) {
+        double avg = UsersRepository.getUserList()
+                .stream()
+                .filter(user -> user.getAge() >= 40)
+                .mapToInt(p -> p.getNumberOfPost())
+                .average()
+                .getAsDouble();
+        return avg;
+
+    }
+
+    public static double getAverageNumberOfPostsForPeopleYoungerThanFortyYearsOfAge(List<User> users) {
+        double avg = UsersRepository.getUserList()
+                .stream()
+                .filter(user -> user.getAge() < 40)
+                .mapToInt(p -> p.getNumberOfPost())
+                .average()
+                .getAsDouble();
+        return avg;
+
+    }
+
+    public static void main(String[] args) {
+        double olderOrEqualToFortyYearsOfAge = getAverageNumberOfPostsForPeopleOlderOrEqualToFortyYearsOfAge(new ArrayList<>());
+        System.out.println(olderOrEqualToFortyYearsOfAge);
+
+        double youngerThanFortyYearsOfAge = getAverageNumberOfPostsForPeopleYoungerThanFortyYearsOfAge(new ArrayList<>());
+        System.out.println(youngerThanFortyYearsOfAge);
+
+
+    }
 }
--- kodilla-stream/src/test/java/com/kodilla/stream/homework/ForumStatsTest.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/homework/ForumStatsTest.java
@@ -1,4 +1,30 @@
+package com.kodilla.stream.homework;
+
+import com.kodilla.stream.User;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class ForumStatsTest {
-  
+    @Test
+    public  void testGetAverageNumberOfPostsForPeopleOlderOrEqualToFortyYearsOfAge(){
+        ForumStats forumStats = new ForumStats();
+        double avg = ForumStats.getAverageNumberOfPostsForPeopleOlderOrEqualToFortyYearsOfAge(new ArrayList<>());
+
+        assertEquals(2.25,avg,0.5);
+    }
+    @Test
+    public void testGetAverageNumberOfPostsForPeopleYoungerThanFortyYearsOfAge() {
+        ForumStats forumStats = new ForumStats();
+        double avg = ForumStats.getAverageNumberOfPostsForPeopleYoungerThanFortyYearsOfAge(new ArrayList<>());
+
+        assertEquals(2382,avg,1);
+    }
+
+
+
+
 }
