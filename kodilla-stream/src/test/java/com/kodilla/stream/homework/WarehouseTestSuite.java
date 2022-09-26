a56b4c3ba7d013ff1eceafe7e2372402f7e7f140
Bartek Kuropatwa <bartek.kuropatwa@gmail.com>
2022-09-26 19:17:14 +0200

    kodilla 6.5

--- kodilla-stream/src/test/java/com/kodilla/stream/homework/WarehouseTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/homework/WarehouseTestSuite.java
@@ -5,7 +5,16 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class WarehouseTestSuite {
+    @Test
+    public void testGetOrder() throws OrderDoesntExistException {
+        Warehouse warehouse = new Warehouse();
 
+        warehouse.addOrder(new Order("7"));
+        String result = warehouse.getOrder("7").getNumber();
+        assertEquals(7,result);
+
+
+    }
     @Test
     public void testGetOrder_withException() {
         Warehouse warehouse = new Warehouse();
