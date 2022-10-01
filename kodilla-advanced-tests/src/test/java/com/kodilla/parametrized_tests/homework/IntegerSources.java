a9052cfcb371f1b3b7d05f4093fc5472993ad5c1
Bartek Kuropatwa <bartek.kuropatwa@gmail.com>
2022-10-01 15:43:15 +0200

    kodilla

---
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/IntegerSources.java
@@ -0,0 +1,18 @@
+package com.kodilla.parametrized_tests.homework;
+
+import org.junit.jupiter.params.provider.Arguments;
+
+import java.util.stream.Stream;
+
+public class IntegerSources {
+    static Stream<Arguments> provideIntegerForTestingBMI() {
+        return Stream.of(
+                Arguments.of(1.78, 85 ,"Overweight"),
+                Arguments.of(1.70, 100,"Obese Class I (Moderately obese)"),
+                Arguments.of(1.88, 170, "Obese Class IV (Morbidly Obese)")
+
+
+
+        );
+    }
+}
