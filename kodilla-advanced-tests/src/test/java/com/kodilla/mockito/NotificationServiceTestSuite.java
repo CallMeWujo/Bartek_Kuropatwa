d0a11b7c22c9c89da0832dd6a3f248183e428fcb
Bartek Kuropatwa <bartek.kuropatwa@gmail.com>
2022-10-05 22:26:41 +0200

    kodilla

--- kodilla-advanced-tests/src/test/java/com/kodilla/mockito/NotificationServiceTestSuite.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/mockito/NotificationServiceTestSuite.java
@@ -3,15 +3,21 @@ package com.kodilla.mockito;
 import com.kodilla.notification.Client;
 import com.kodilla.notification.Notification;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 
 public class NotificationServiceTestSuite {
+    NotificationService notificationService = new NotificationService();
+    Client client = Mockito.mock(Client.class);
+    Client secondClient = Mockito.mock(Client.class);
+    Client thirdClient = Mockito.mock(Client.class);
+    Notification notification = Mockito.mock(Notification.class);
 
     @Test
     public void notSubscribedClientShouldNotReceiveNotification() {
-        NotificationService notificationService = new NotificationService();
+
         Client client = Mockito.mock(Client.class);
-        Notification notification = Mockito.mock(Notification.class);
+
 
         notificationService.sendNotification(notification);
         Mockito.verify(client, Mockito.never()).receive(notification);
@@ -20,10 +26,10 @@ public class NotificationServiceTestSuite {
 
     @Test
     public void subscribedClientShouldReceiveNotification() {
-        NotificationService notificationService = new NotificationService();
-        Client client = Mockito.mock(Client.class);
+
+
         notificationService.addSubscriber(client);
-        Notification notification = Mockito.mock(Notification.class);
+
 
         notificationService.sendNotification(notification);
         Mockito.verify(client, Mockito.times(1)).receive(notification);
@@ -33,15 +39,47 @@ public class NotificationServiceTestSuite {
     @Test
     public void notificationShouldBeSentToAllSubscribedClients() {
 
+
+
+
+        notificationService.addSubscriber(client);
+        notificationService.addSubscriber(secondClient);
+        notificationService.addSubscriber(thirdClient);
+
+
+        notificationService.sendNotification(notification);
+
+        Mockito.verify(client, Mockito.times(1)).receive(notification);
+        Mockito.verify(secondClient, Mockito.times(1)).receive(notification);
+        Mockito.verify(thirdClient, Mockito.times(1)).receive(notification);
+
     }
 
     @Test
     public void shouldSendOnlyOneNotificationToMultiTimeSubscriber() {
+        addSubscribers(client, secondClient, thirdClient);
+
+
+
+        notificationService.sendNotification(notification);
+        Mockito.verify(client, Mockito.times(1)).receive(notification);
 
     }
 
     @Test
     public void unsubscribedClientShouldNotReceiveNotification() {
 
+        notificationService.addSubscriber(client);
+
+        notificationService.removeSubscriber(client);
+
+        notificationService.sendNotification(notification);
+        Mockito.verify(client, Mockito.never()).receive(notification);
+
+    }
+    private void addSubscribers(Client... clients) {
+        for (Client currentClient : clients) {
+            notificationService.addSubscriber(currentClient);
+        }
     }
 }
