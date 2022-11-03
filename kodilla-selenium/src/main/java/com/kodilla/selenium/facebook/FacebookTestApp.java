5716ece70513dd0d1de2b9d27dc0b5d8be0c00e1
Bartek Kuropatwa <bartek.kuropatwa@gmail.com>
2022-11-03 20:43:57 +0100

    kodilla

--- kodilla-selenium/src/main/java/com/kodilla/selenium/facebook/FacebookTestApp.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/facebook/FacebookTestApp.java
@@ -1,2 +1,37 @@
-package com.kodilla.selenium.facebook;public class FacebookTestApp {
+package com.kodilla.selenium.facebook;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class FacebookTestApp {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "C:\\selenium-drivers\\Chrome\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.facebook.com/");
+
+        WebElement cookies = driver.findElement(By.xpath("//button[2]"));
+        cookies.click();
+
+
+        WebElement newAccount = driver.findElement(By.xpath("//div[5]"));
+        newAccount.click();
+        Thread.sleep(4000);
+
+        WebElement yearCombo = driver.findElement(By.xpath("//*[@id=\"birthday_wrapper\"]/div/span/span/select[3]"));
+        Select yearSelect = new Select(yearCombo);
+        yearSelect = new Select(yearCombo);
+        yearSelect.selectByIndex(5);
+
+
+
+
+    }
+
+
 }
