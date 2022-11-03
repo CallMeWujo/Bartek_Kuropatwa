c13d54da26b15e183cd20291948af67696f9b4c9
Bartek Kuropatwa <bartek.kuropatwa@gmail.com>
2022-11-03 13:57:01 +0100

    kodilla

--- kodilla-selenium/src/main/java/com/kodilla/selenium/google/GoogleExample.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/google/GoogleExample.java
@@ -1,2 +1,27 @@
-package com.kodilla.selenium.google;public class GoogleExample {
+package com.kodilla.selenium.google;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class GoogleExample {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:\\selenium-drivers\\Chrome\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+
+        WebElement termsOfUse = driver.findElement(By.id("L2AGLb"));
+        termsOfUse.click();
+
+        WebElement inputField = driver.findElement(By.name("q"));
+        inputField.sendKeys("Kodilla");
+        inputField.submit();
+
+
+
+
+    }
 }
