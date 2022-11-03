7d4375c892e05176a159167a3d8891e3ee0e0437
Bartek Kuropatwa <bartek.kuropatwa@gmail.com>
2022-11-03 13:56:37 +0100

    kodilla 13.3

---
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/ebay/EBayTestingApp.java
@@ -0,0 +1,35 @@
+package com.kodilla.selenium.ebay;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class EBayTestingApp {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:\\selenium-drivers\\Chrome\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.ebay.com/");
+
+        WebElement cookies = driver.findElement(By.name("_nkw"));
+        cookies.sendKeys("Laptop");
+        cookies.submit();
+
+
+
+
+
+
+
+
+    }
+}
+
+
+
+
+
+
