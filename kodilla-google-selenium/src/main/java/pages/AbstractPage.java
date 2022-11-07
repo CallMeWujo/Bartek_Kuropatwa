8be10f7c730ed3409b97f08cce2b1f9d9431e1ce
Bartek Kuropatwa <bartek.kuropatwa@gmail.com>
2022-11-07 23:16:10 +0100

    kodilla

--- kodilla-google-selenium/src/main/java/pages/AbstractPage.java
+++ kodilla-google-selenium/src/main/java/pages/AbstractPage.java
@@ -1,2 +1,11 @@
-package pages;public class AbstractPage {
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public abstract class AbstractPage {
+    protected WebDriver driver;
+
+    public AbstractPage(WebDriver driver) {
+        this.driver = driver;
+    }
 }
