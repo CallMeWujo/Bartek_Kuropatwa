b7c8087780c74e0b3f5dcf542e1526b9c5b44519
Bartek Kuropatwa <bartek.kuropatwa@gmail.com>
2023-03-21 20:49:35 +0100

    kodilla 15.4

--- kodilla-code-coverage/src/test/java/com/kodilla/hibernate/tasklist/repository/TaskListRepositoryTest.java
+++ kodilla-code-coverage/src/test/java/com/kodilla/hibernate/tasklist/repository/TaskListRepositoryTest.java
@@ -1,2 +1,36 @@
-package com.kodilla.hibernate.tasklist.repository;public class TaskListRepositoryTest {
+package com.kodilla.hibernate.tasklist.repository;
+
+import com.kodilla.hibernate.tasklist.TaskList;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+public class TaskListRepositoryTest {
+
+    @Autowired
+    private TaskListRepository taskListRepository;
+    private static final String LISTNAME = "Test: Hibernate";
+    private static final String DESCRYPTION = "First task";
+
+    @Test
+    void testFindByListName() {
+        //Given
+        TaskList taskList = new TaskList(LISTNAME,DESCRYPTION);
+        taskListRepository.save(taskList);
+        String name = taskList.getListName();
+
+        //When
+        List<TaskList> readTaskList = taskListRepository.findByListName(name);
+
+        //Then
+        Assertions.assertEquals(7,readTaskList.size());
+
+        //CleanUp
+        int id = readTaskList.get(0).getId();
+        taskListRepository.deleteById(id);
+    }
 }
