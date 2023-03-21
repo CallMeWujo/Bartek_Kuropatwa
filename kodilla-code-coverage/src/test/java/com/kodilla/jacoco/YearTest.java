cbeb7cdc7703d271ea3d5447f55c0b9351d7417d
Bartek Kuropatwa <bartek.kuropatwa@gmail.com>
2023-03-21 20:41:58 +0100

    kodilla 15.2

--- kodilla-code-coverage/src/test/java/com/kodilla/jacoco/YearTest.java
+++ kodilla-code-coverage/src/test/java/com/kodilla/jacoco/YearTest.java
@@ -2,30 +2,60 @@ package com.kodilla.jacoco;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class YearTest {
 
     @Test
-      void shouldBeLeapYearIfDivisibleBy400 () {
+     void shouldBeLeapYearIfDivisibleBy400() {
+
+        //given
         Year year = new Year(1600);
+
+        //when
         boolean isLeapYear = year.isLeap();
+
+        //then
         assertTrue(isLeapYear);
     }
 
     @Test
-    void shouldNotBeLeapYearIfIsNotDivisibleBy4() {
-        Year year = new Year(1601);
+     void shouldBeLeapYearIfDivisibleBy4AndNotDivisibleBy100() {
+
+        //given
+        Year year = new Year(1604);
+
+        //when
         boolean isLeapYear = year.isLeap();
+
+        //then
+        assertTrue(isLeapYear);
+    }
+
+    @Test
+     void shouldNotBeLeapYearIfNotDivisibleBy4() {
+
+        //given
+        Year year = new Year(1603);
+
+        //when
+        boolean isLeapYear = year.isLeap();
+
+        //then
         assertFalse(isLeapYear);
     }
 
     @Test
-    void shouldBeLeapYearIfDivisibleBy100() {
-        Year year = new Year(1600);
+     void shouldNotBeLeapYearIfNotDivisibleBy400() {
+
+        //given
+        Year year = new Year(1700);
+
+        //when
         boolean isLeapYear = year.isLeap();
-        assertTrue(isLeapYear);
+
+        //then
+        assertFalse(isLeapYear);
     }
-
-
 }
