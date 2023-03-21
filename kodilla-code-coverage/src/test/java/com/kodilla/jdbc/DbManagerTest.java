ce10a4bf7d46dff3b9b24f8667fedf59ee1c6698
Bartek Kuropatwa <bartek.kuropatwa@gmail.com>
2023-03-21 20:43:18 +0100

    kodilla 15.3

--- kodilla-code-coverage/src/test/java/com/kodilla/jdbc/DbManagerTest.java
+++ kodilla-code-coverage/src/test/java/com/kodilla/jdbc/DbManagerTest.java
@@ -1,4 +1,135 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.kodilla.jdbc;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.AbstractMap;
+import java.util.List;
+
 class DbManagerTest {
-  
+    private static DbManager dbManager;
+
+    @BeforeAll
+    public static void setup() throws SQLException {
+        dbManager = dbManager.getInstance();
+    }
+
+    @Test
+    void testConnect() {
+        //Given
+        //When
+        //Then
+        Assertions.assertNotNull(dbManager.getConnection());
+    }
+
+    @Test
+    void testSelectedUsers() throws SQLException {
+        //Given
+        String countQuery = "SELECT COUNT(*) FROM USERS";
+        Statement statement = createStatement();
+        ResultSet rs = statement.executeQuery(countQuery);
+        int count = getRowsCount(rs);
+        insertUsers(statement);
+
+        //When
+        String sqlQuery = "SELECT * FROM USERS";
+        statement = createStatement();
+        rs = statement.executeQuery(sqlQuery);
+
+        //Then
+        int counter = getResultsCount(rs);
+        int expected = count + 5;
+        Assertions.assertEquals(expected, counter);
+
+        rs.close();
+        statement.close();
+
+    }
+    private Statement createStatement() throws SQLException {
+        return dbManager.getConnection().createStatement();
+    }
+
+    private static final List<AbstractMap.SimpleEntry<String,String>> USERS = List.of(
+            new AbstractMap.SimpleEntry<>("Zara", "Ali"),
+            new AbstractMap.SimpleEntry<>("Otman", "Use"),
+            new AbstractMap.SimpleEntry<>("Mark", "Boq"),
+            new AbstractMap.SimpleEntry<>("Uli", "Wimer"),
+            new AbstractMap.SimpleEntry<>("Oli", "Kosiw")
+    );
+
+    private void insertUsers(Statement statement) throws SQLException {
+        for (AbstractMap.SimpleEntry<String, String> user : USERS) {
+            statement.executeUpdate(
+                    String.format("INSERT INTO USERS(FIRSTNAME, LASTNAME) VALUES ('%s', '%s')",
+                    user.getKey(),
+                    user.getValue()
+                    )
+            );
+        }
+    }
+
+    private static int getResultsCount(ResultSet rs) throws SQLException {
+        int counter = 0;
+        while (rs.next()) {
+            System.out.printf("%d, %s, %s%n",
+                    rs.getInt("ID"),
+                    rs.getString("FIRSTNAME"),
+                    rs.getString("LASTNAME"));
+            counter ++;
+        }
+        return counter;
+    }
+
+    private static int getRowsCount(ResultSet rs) throws SQLException {
+        int count = 0;
+        while (rs.next()) {
+            count = rs.getInt("COUNT(*)");
+        }
+        return count;
+    }
+
+    @Test
+    void testSelectUsersAndPosts() throws SQLException {
+        //Given
+        DbManager dbManager = DbManager.getInstance();
+        String countQuery = "SELECT U.FIRSTNAME, U.LASTNAME, COUNT(*) FROM USERS U\n" +
+                "JOIN POSTS P ON U.ID = P.USER_ID\n" +
+                "GROUP BY P.USER_ID\n" +
+                "HAVING COUNT(*) > 1;";
+        Statement statement = dbManager.getConnection().createStatement();
+        ResultSet rs = statement.executeQuery(countQuery);
+
+        int count = 0;
+
+        while(rs.next()) {
+            System.out.println(rs.getString("FIRSTNAME") + ", " +
+                    rs.getString("LASTNAME"));
+            count++;
+        }
+
+        // When
+        String sqlQuery = "SELECT U.FIRSTNAME, U.LASTNAME, COUNT(*) FROM USERS U\n" +
+                "JOIN POSTS P ON U.ID = P.USER_ID\n" +
+                "GROUP BY P.USER_ID\n" +
+                "HAVING COUNT(*) > 1;";
+        statement = dbManager.getConnection().createStatement();
+        rs = statement.executeQuery(sqlQuery);
+
+        //Then
+        int counter = 0;
+        while(rs.next()) {
+            System.out.println(rs.getString("FIRSTNAME") + ", " +
+                    rs.getString("LASTNAME"));
+            counter++;
+        }
+        int expected = count ;
+        Assertions.assertEquals(expected, counter);
+
+        rs.close();
+        statement.close();
+    }
 }
