7ce1503887adc17ceffb28fb712d3ac7bbf85e42
Bartek Kuropatwa <bartek.kuropatwa@gmail.com>
2023-03-28 20:58:07 +0200

    kodilla 16

--- kodilla-testcontainers/src/test/java/com/kodilla/testcontainers/ApplicationTest.java
+++ kodilla-testcontainers/src/test/java/com/kodilla/testcontainers/ApplicationTest.java
@@ -2,12 +2,13 @@ package com.kodilla.testcontainers;
 
 
 import org.junit.Rule;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testcontainers.containers.BrowserWebDriverContainer;
+import org.testcontainers.containers.DefaultRecordingFileFactory;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.Network;
 import org.testcontainers.images.builder.ImageFromDockerfile;
@@ -16,8 +17,8 @@ import org.testcontainers.shaded.org.apache.commons.io.FileUtils;
 import java.io.File;
 import java.io.IOException;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.testcontainers.containers.BrowserWebDriverContainer.VncRecordingMode.SKIP;
+import static org.junit.Assert.assertEquals;
+import static org.testcontainers.containers.BrowserWebDriverContainer.VncRecordingMode.RECORD_ALL;
 
 public class ApplicationTest {
     @Rule
@@ -41,15 +42,17 @@ public class ApplicationTest {
     public BrowserWebDriverContainer chrome =
             new BrowserWebDriverContainer<>()
                     .withNetwork(network)
-                    .withRecordingMode(SKIP, null)
-                    .withCapabilities(new ChromeOptions());
+                    .withRecordingMode(RECORD_ALL, new File("./build/"))
+                    .withCapabilities(new ChromeOptions())
+                    .withRecordingFileFactory(new DefaultRecordingFileFactory());
+
 
     @Test
     public void customImageTest() throws InterruptedException, IOException {
         RemoteWebDriver driver = chrome.getWebDriver();
         driver.get("http://my-server/");
 
-        Thread.sleep(15000);
+        Thread.sleep(5000);
 
         File screenshot = driver.getScreenshotAs(OutputType.FILE);
         FileUtils.copyFile(screenshot, new File("./build/screenshots/" + screenshot.getName()));
